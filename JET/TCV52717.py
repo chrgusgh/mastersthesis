359a7e26d784088ff54a4375ea35f29df62a2080
Christian Gustavsson <chrgus@proton.me>
2024-02-14 11:27:58 +0100

    Moved input data from TCV52717.py to a new inputs.py file. Moreover, have added code for currents densities, but not yet fully implemented since psi_apRp data is weird

--- JET/TCV52717.py
+++ JET/TCV52717.py
@@ -10,55 +10,9 @@ from driver import SCAN_NONE, SCAN_NEON, SCAN_NRE, SCAN_CURRENT
 
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.constants import k, e as kb, e
 import sys
 
-sys.path.append('/home/christiang/Skrivbord/DREAM/tools/eqget')
-from EQDSK import EQDSK
-import h5py
-
-# JET data (wikipedia)
-#a             = 1.25
-#R0            = 2.96
-#B0            = 3.45
-plasma_volume = 100 # m3
-Ti            = 300 #  assumption for impurities, K
-kb            = kb # Boltzmann constant
-
-# Pick one
-SHOT = 1
-
-fp      = '../JETdata/DDB_Runaways_ArBt_scan.h5'
-pulses  = ['85021', '85445', '85450', '85451', '85453', '85943']
-f       = h5py.File(fp, 'r')
-p0      = f[pulses[SHOT]]
-eq      = EQDSK(f'../JETdata/g_JET_ehtr_{pulses[SHOT]}_t62.3990_62.4010', override_psilim=2e-3)
-
-n0r     = p0['R_Ne_profile_LIDAR_m'][:] - eq.R0
-n0r_pos = np.where(n0r >= 0)
-n0r     = n0r[n0r_pos]
-T0r     = p0['R_Te_profile_ECE_m'][:] - eq.R0
-T0r_pos = np.where(T0r >= 0)
-T0r     = T0r[T0r_pos]
-n0      = p0['Ne_profile_LIDAR_mm3'][:]
-n0      = n0[n0r_pos]
-T0      = p0['Te_profile_ECE_eV'][:]
-T0      = T0[T0r_pos]
-Ip0     = -p0['Ip_A'][:] # positive current
-D2      = p0['D2_Pam3'][:]
-Ar      = p0['Ar_Pam3'][:]
-
-# Convert mag equlib data to h5
-mag_eq_fn = f'mag_eq_{pulses[SHOT]}_data.h5'
-#eq.save_LUKE(mag_eq_fn)
-
-def impurity_density(impurity):
-    N = impurity / (kb * Ti)
-    n_i = N / plasma_volume
-    return n_i
-
-n_D2 = impurity_density(D2)
-n_Ar = impurity_density(Ar)
+import inputs as inputs
 
 def get_settings(argv):
     args = parse_args(argv)
@@ -76,22 +30,20 @@ def get_settings(argv):
         'impurities': [
             #{'name': 'Ne', 'Z': 10, 'n': bar(6.2)}
             #{'name': 'Ne', 'Z': 10, 'n': 1e19},
-            {'name': 'Ar', 'Z': 18, 'n': n_Ar},
-            {'name': 'D2', 'Z': 1, 'n': n_D2}
-        ], # Impurities
-        'Ip0' : Ip0,
-        #'nre0': 0,
-        #'nre0_r': 0,
-        'n0': n0,
-        'n0r': n0r,
-        'j0': lambda r : np.ones(r.shape),
-        'j0r': None,
-        #'T0': 1e3,
-        #'T0': lambda r : 1000*np.exp(-((r/a)/0.4653)**2),
-        'T0': T0,
-        'T0r': T0r,
-        #'T0': DDB['Te_profile_ECE_eV'][:][:,0],
-        #'T0r': DDB['R_Te_profile_ECE_m'][:][:,0],
+            {'name': 'Ar', 'Z': 18, 'n': inputs.n_Ar},
+            {'name': 'D2', 'Z': 1, 'n': inputs.n_D2}
+        ],
+        'Ip0' : inputs.Ip0,
+        'nre0': 0,
+        'nre0_r': 0,
+        'n0': inputs.n0,
+        'n0r': inputs.n0r,
+        #'j0': lambda r : np.ones(r.shape),
+        #'j0r': None,
+        #'j0': inputs.j_par_at_Bmin,
+        #'j0r': inputs.r,
+        'T0': inputs.T0,
+        'T0r': inputs.T0r,
         'E0': 1,
         'E0r': None,
         'tauwall': None,
@@ -115,7 +67,7 @@ def get_settings(argv):
 
 
 def run_disruption_simulation(args, settings):
-    ds = generate_baseline(equilibrium=None if args.cylindrical else f'../JETdata/{mag_eq_fn}',
+    ds = generate_baseline(equilibrium=None if args.cylindrical else f'../JETdata/{inputs.mag_eq_fn}',
         runInit=(args.runfrom <= 0), verboseInit=(0 in args.verbose), **settings)
 
     doMain, _ = simulate(ds, **settings)
---
+++ JET/inputs.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+import argparse
+import copy
+from driver import a, bar, generate_baseline, parse_args, simulate, MODE_FLUID, MODE_ISOTROPIC
+from driver import SCAN_NONE, SCAN_NEON, SCAN_NRE, SCAN_CURRENT
+# from visualize import disruption_summary
+
+# from scans import doscan
+
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.constants import k, e as kb, e
+import sys
+
+sys.path.append('/home/christiang/Skrivbord/DREAM/tools/eqget')
+
+from EQDSK import EQDSK
+
+# Pick one
+SHOT = 5
+
+fp      = '../JETdata/DDB_Runaways_ArBt_scan.h5'
+pulses  = ['85021', '85445', '85450', '85451', '85453', '85943']
+f       = h5py.File(fp, 'r')
+p0      = f[pulses[SHOT]]
+
+eqdsk   = EQDSK(f'../JETdata/g_JET_ehtr_{pulses[SHOT]}_t62.3990_62.4010', override_psilim=2e-3)
+
+# Convert mag equlib data to h5
+mag_eq_fn = f'mag_eq_{pulses[SHOT]}_data.h5'
+#eq.save_LUKE(mag_eq_fn)
+# JET Parameters and more
+
+equil = h5py.File(f'../JETdata/{mag_eq_fn}', 'r')['equil']
+ptx = equil['ptx'][:]
+psi_apRp = equil['psi_apRp'][:]
+# Get each psi_apRp
+psi_apRp      = np.array([arr.item() for arr in psi_apRp])
+print(psi_apRp)
+
+a             = ptx[-1,-1] # 0.901
+R0            = eqdsk.R0
+psi_n         = psi_apRp #* a / R0
+print(psi_n)
+#B0            = 3.45
+plasma_volume = 100 # m3
+Ti            = 300 #  assumption for impurities, K
+kb            = kb # Boltzmann constant
+
+n0r     = p0['R_Ne_profile_LIDAR_m'][:] - R0
+n0r_pos = np.where(n0r >= 0)
+n0r     = n0r[n0r_pos]
+T0r     = p0['R_Te_profile_ECE_m'][:] - R0
+T0r_pos = np.where(T0r >= 0)
+T0r     = T0r[T0r_pos]
+n0      = p0['Ne_profile_LIDAR_mm3'][:]
+n0      = n0[n0r_pos]
+T0      = p0['Te_profile_ECE_eV'][:]
+T0      = T0[T0r_pos]
+Ip0     = -p0['Ip_A'][:] # positive current
+D2      = p0['D2_Pam3'][:]
+Ar      = p0['Ar_Pam3'][:]
+
+#psi_n_tmp = psi_n / psi_n[-1]
+#psi_n = psi_n_tmp
+#print(psi_n)
+
+#R, Z          = eqdsk.get_flux_surface(psi_n[0])
+#j_tor         = eqdsk.get_Jtor(R, Z)
+#j_tor_at_Bmin = eqdsk.get_Jtor_at_Bmin(psi_n)
+#j_par_at_Bmin = eqdsk.get_Jpar_at_Bmin(psi_n)
+#r             = eqdsk.get_r(psi_n)
+#print(j_par_at_Bmin)
+#print(r)
+
+def impurity_density(impurity):
+    """
+    Computes impurity density from the gas law
+    
+    Input:
+    - impurity: in units Pa m^3
+    """
+    N = impurity / (kb * Ti)
+    n_i = N / plasma_volume
+    return n_i
+
+n_D2 = impurity_density(D2)
+n_Ar = impurity_density(Ar)
